c4fbe1769a197d1b3318a4ba50d58b4f49005dbf
gyeongyeol-choi <gyeongyeol_choi@tmax.co.kr>
2022-06-20 18:28:39 +0900

    [etc] Spring Project 구성 초기화

---
+++ src/test/java/com/tmax/tcp/TcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmax.tcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
